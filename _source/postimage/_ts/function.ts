f6af2149e3d8446f1a82c05651598e411993cc65
OMD_sadakitchen <contact@onemoredot.com>
2015-11-14 08:39:07 +0900

    スマホの方でボールを投げられるように

--- _source/postimage/_ts/function.ts
+++ _source/postimage/_ts/function.ts
@@ -1,32 +1,65 @@
 /// <reference path="jquery.d.ts" />
-class Ball{
+class Ball {
 	private posX: number;
 	private posY: number;
 	private x1: number;
 	private y1: number;
 	private x2: number;
 	private y2: number;
-	private size: number;
 	private spdX: number = 0;
 	private spdY: number = 0;
 	private isHold: boolean = false;
+	private timerToken: number;
+	public isSupportTouch: boolean = ('ontouchstart' in window);
+	public dom: HTMLElement;
 
-	constructor(x: number, y: number, _size: number) {
+	constructor( _dom: HTMLElement, x: number, y: number, public size: number) {
+
+		// this.isSupportTouch = ('ontouchstart' in window);
+		console.log("isSupportTouch:" +this.isSupportTouch);
+
 		this.posX = x;
 		this.posY = y;
-		this.size = _size;
+
+		this.dom = _dom;
+
+		var EVENTNAME_TOUCHSTART:string = this.isSupportTouch ? 'touchstart' : 'mousedown';
+		var EVENTNAME_TOUCHMOVE:string = this.isSupportTouch ? 'touchmove' : 'mousemove';
+		var EVENTNAME_TOUCHEND:string = this.isSupportTouch ? 'touchend' : 'mouseup';
+
+		// プレスイベント
+		this.dom.addEventListener(EVENTNAME_TOUCHSTART, this.onPress,false);
+		this.dom.addEventListener(EVENTNAME_TOUCHMOVE, this.enterFrame,false);
+		this.dom.addEventListener(EVENTNAME_TOUCHEND, this.onRelease,false);
+
+		this.timerToken = setInterval((e) => this.enterFrame(e),33);
 	}
 
-	public enterFrame():void
-	{
+	private enterFrame = (e: any): void => {
 		if (this.isHold === true) {
+			// console.log(e);
+			var clX: number = (this.isSupportTouch ? e.changedTouches[0].clientX : e.pageX);
+			var clY: number = (this.isSupportTouch ? e.changedTouches[0].clientY : e.pageY);
+
 			this.x1 = this.x2;
 			this.y1 = this.y2;
-			this.x2 = this.posX;
-			this.y2 = this.posY;
+			// this.x2 = this.posX;
+			// this.y2 = this.posY;
+			this.x2 = clX;
+			this.y2 = clY;
 			this.spdX = (this.x2 - this.x1);
 			this.spdY = (this.y2 - this.y1);
+
+			// this.isSupportTouch = ('ontouchstart' in window);
+			// console.log(this.isSupportTouch);
+			// console.log("clY:" +clY);
+			var _posX: number = (clX - this.size * 0.5);
+			var _posY: number = (clY - this.size * 0.5);
+			console.log("_posX:" +_posX);
+			this.dom.style.left = _posX+"px";
+			this.dom.style.top = _posY+"px";
 		} else {
+			// console.log(this.spdX);
 			if (this.posX > 500 - this.size / 2) {
 				this.posX = 500 - this.size / 2;
 				this.spdX = this.spdX * -1;
@@ -45,17 +78,27 @@ class Ball{
 			}
 			this.posX += this.spdX;
 			this.posY += this.spdY;
+
+			this.dom.style.left = this.posX+"px";
+			this.dom.style.top = this.posY+"px";
 		}
 	}
-	public onPress():void
-	{
+	private onPress = (e :any) => {
+		console.log("onPress");
+		if(e !== null){
+			e.preventDefault();
+		}
 		this.isHold = true;
+
+		clearInterval(this.timerToken);
 	}
-	public onRelease():void
-	{
+	private onRelease = () => {
+		console.log("onRelease");
 		this.isHold = false;
+
+		this.timerToken = setInterval((e) => this.enterFrame(e),33);
 	}
 }
 $(document).ready(() => {
-	
+
 });
--- _source/postimage/js/function.js
+++ _source/postimage/js/function.js
@@ -1,49 +1,85 @@
 /// <reference path="jquery.d.ts" />
 var Ball = (function () {
-    function Ball(x, y, _size) {
+    function Ball(_dom, x, y, size) {
+        var _this = this;
+        this.size = size;
         this.spdX = 0;
         this.spdY = 0;
         this.isHold = false;
+        this.isSupportTouch = ('ontouchstart' in window);
+        this.enterFrame = function (e) {
+            if (_this.isHold === true) {
+                // console.log(e);
+                var clX = (_this.isSupportTouch ? e.changedTouches[0].clientX : e.pageX);
+                var clY = (_this.isSupportTouch ? e.changedTouches[0].clientY : e.pageY);
+                _this.x1 = _this.x2;
+                _this.y1 = _this.y2;
+                // this.x2 = this.posX;
+                // this.y2 = this.posY;
+                _this.x2 = clX;
+                _this.y2 = clY;
+                _this.spdX = (_this.x2 - _this.x1);
+                _this.spdY = (_this.y2 - _this.y1);
+                // this.isSupportTouch = ('ontouchstart' in window);
+                // console.log(this.isSupportTouch);
+                // console.log("clY:" +clY);
+                var _posX = (clX - _this.size * 0.5);
+                var _posY = (clY - _this.size * 0.5);
+                console.log("_posX:" + _posX);
+                _this.dom.style.left = _posX + "px";
+                _this.dom.style.top = _posY + "px";
+            }
+            else {
+                // console.log(this.spdX);
+                if (_this.posX > 500 - _this.size / 2) {
+                    _this.posX = 500 - _this.size / 2;
+                    _this.spdX = _this.spdX * -1;
+                }
+                if (_this.posX < 0 + _this.size / 2) {
+                    _this.posX = 0 + _this.size / 2;
+                    _this.spdX = _this.spdX * -1;
+                }
+                if (_this.posY > 400 - _this.size / 2) {
+                    _this.posY = 400 - _this.size / 2;
+                    _this.spdY = _this.spdY * -1;
+                }
+                if (_this.posY < 0 + _this.size / 2) {
+                    _this.posY = 0 + _this.size / 2;
+                    _this.spdY = _this.spdY * -1;
+                }
+                _this.posX += _this.spdX;
+                _this.posY += _this.spdY;
+                _this.dom.style.left = _this.posX + "px";
+                _this.dom.style.top = _this.posY + "px";
+            }
+        };
+        this.onPress = function (e) {
+            console.log("onPress");
+            if (e !== null) {
+                e.preventDefault();
+            }
+            _this.isHold = true;
+            clearInterval(_this.timerToken);
+        };
+        this.onRelease = function () {
+            console.log("onRelease");
+            _this.isHold = false;
+            _this.timerToken = setInterval(function (e) { return _this.enterFrame(e); }, 33);
+        };
+        // this.isSupportTouch = ('ontouchstart' in window);
+        console.log("isSupportTouch:" + this.isSupportTouch);
         this.posX = x;
         this.posY = y;
-        this.size = _size;
+        this.dom = _dom;
+        var EVENTNAME_TOUCHSTART = this.isSupportTouch ? 'touchstart' : 'mousedown';
+        var EVENTNAME_TOUCHMOVE = this.isSupportTouch ? 'touchmove' : 'mousemove';
+        var EVENTNAME_TOUCHEND = this.isSupportTouch ? 'touchend' : 'mouseup';
+        // プレスイベント
+        this.dom.addEventListener(EVENTNAME_TOUCHSTART, this.onPress, false);
+        this.dom.addEventListener(EVENTNAME_TOUCHMOVE, this.enterFrame, false);
+        this.dom.addEventListener(EVENTNAME_TOUCHEND, this.onRelease, false);
+        this.timerToken = setInterval(function (e) { return _this.enterFrame(e); }, 33);
     }
-    Ball.prototype.enterFrame = function () {
-        if (this.isHold === true) {
-            this.x1 = this.x2;
-            this.y1 = this.y2;
-            this.x2 = this.posX;
-            this.y2 = this.posY;
-            this.spdX = (this.x2 - this.x1);
-            this.spdY = (this.y2 - this.y1);
-        }
-        else {
-            if (this.posX > 500 - this.size / 2) {
-                this.posX = 500 - this.size / 2;
-                this.spdX = this.spdX * -1;
-            }
-            if (this.posX < 0 + this.size / 2) {
-                this.posX = 0 + this.size / 2;
-                this.spdX = this.spdX * -1;
-            }
-            if (this.posY > 400 - this.size / 2) {
-                this.posY = 400 - this.size / 2;
-                this.spdY = this.spdY * -1;
-            }
-            if (this.posY < 0 + this.size / 2) {
-                this.posY = 0 + this.size / 2;
-                this.spdY = this.spdY * -1;
-            }
-            this.posX += this.spdX;
-            this.posY += this.spdY;
-        }
-    };
-    Ball.prototype.onPress = function () {
-        this.isHold = true;
-    };
-    Ball.prototype.onRelease = function () {
-        this.isHold = false;
-    };
     return Ball;
 })();
 $(document).ready(function () {
